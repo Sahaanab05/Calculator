a78e8708103e5381a13f79831bffbf39334dcf34
Sahaanab <theextractor1@gmail.com>
2021-06-10 22:28:34 +0545

    First

---
+++ calc.py
@@ -0,0 +1,7 @@
+a = input(" Hey please enter a number")
+b = input("Anotehr one please! ")
+
+sum = a + b #perfroms addition
+diff = a - b #performs subtraction
+mul = a * b #perfroms multiplication
+div = a / b #performs diivision
